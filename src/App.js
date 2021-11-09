f22736264ef1fc910c2b51e35996ea20374c8fc1
jill <jill@venuslive.com.tw>
2021-11-09 10:02:22 +0800

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Home from './pages/Home';
 import './index.css';
 
 function App() {
   return (
-    // TODO: Route and Switch
-    <div className="App">
-      Taiwan Tour
-    </div>
+    // <div className="App">
+    <Router>
+      <Switch>
+        <Route path='/' component={Home} exact strict />
+        {/* TODO: replace components */}
+        <Route path='/scenicspot' component={'scenicspot'} exact strict />
+        <Route path='/restaurant' component={'restaurant'} exact strict />
+        <Route path='/activity' component={'activity'} exact strict />
+        <Route
+          path='/scenicspot/:ID'
+          component={(props) => {
+            return <div {...props}></div>;
+          }}
+          exact strict
+        />
+        <Route
+          path='/restaurant/:ID'
+          component={(props) => {
+            return <div {...props}></div>;
+          }}
+          exact strict
+        />
+        <Route
+          path='/activity/:ID'
+          component={(props) => {
+            return <div {...props}></div>;
+          }}
+          exact strict
+        />
+        <Redirect to='/' />
+      </Switch>
+    </Router>
   );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,12 @@
-// TODO:
+import React from 'react';
+
+const Home = () => {
+  return (<>
+    <div>
+      <div>header</div>
+      <img src='' alt='logo' />
+      <div>carousel</div>
+    </div>
+  </>)
+}
+export default Home;
