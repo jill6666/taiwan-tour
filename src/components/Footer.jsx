ee1faee4fb48aa997eff1dfe9167d1539d06b454
Pei Chen Hsieh <shensophia66@gmail.com>
2021-11-14 19:45:09 +0800

    scenicSpot

---
+++ src/components/Footer.jsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import FooterImg from '../assets/footerImg.png';
+import { Grid } from '@material-ui/core';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center">
+            {'Copyright © '}
+            {new Date().getFullYear()}
+            <Typography color="inherit" display='inline'>
+                F2EWiNWiN. All rights reserved.
+            </Typography>
+            {'.'}
+        </Typography>
+    );
+}
+
+function Footer() {
+
+    return (
+        <Box component="footer" sx={{ bgcolor: 'background.paper', py: 6 }} >
+
+            <Grid container xs={12}>
+                <Grid item xs={0} sm={5} />
+                <Grid item xs={3} sm={2}>
+                    <img src={FooterImg} width='300px' height='auto' />
+                </Grid>
+                <Grid item xs={3} sm={3}>
+                    <Copyright />
+                </Grid>
+
+            </Grid>
+        </Box>
+    );
+}
+
+export default Footer;
---
+++ src/components/SpotCardCol.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { Box } from '@material-ui/core';
+
+function SpotCardCol(props) {
+    const { data } = props;
+
+    return (
+
+        <Grid item xs={3.75}>
+
+            <Card sx={{ margin: '30px', }}>
+                <CardActionArea component="a" href="#">
+                    <Box sx={{
+                        height: '160px',
+                        display: 'flex'
+                    }}>
+                        <CardMedia
+                            component="img"
+                            sx={{ width: 160, display: { xs: 'none', sm: 'block' } }}
+                            image={data.Picture.PictureUrl1}
+                        />
+                        <CardContent sx={{
+                            flex: 1, textOverflow: 'ellipsis', my: 2,
+                            overflow: 'hidden',
+                        }}>
+                            <Typography gutterBottom variant="h5" component="div" style={{ fontSize: 20, fontWeight: 'bold' }}>
+                                {data.Name}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" paragraph >
+                                {data.DescriptionDetail}
+                            </Typography>
+                        </CardContent>
+                    </Box>
+                </CardActionArea>
+            </Card>
+
+        </Grid>
+
+    );
+}
+
+SpotCardCol.propTypes = {
+    data: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+        image: PropTypes.string.isRequired,
+    }).isRequired,
+};
+
+export default SpotCardCol;
